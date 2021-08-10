903ce832d573139435fe111332cb188607bed959
williamchou1013 <drizzle60902@gmail.com>
2021-08-10 13:52:34 +0800

    Delete defs.py

--- api/defs.py
+++
@@ -1,7 +0,0 @@
-# API Key
-
-API_KEY = "5d475a6ef1636c3f21c5beae4106ec71-e7dd38f032059393592c6da22df7857e"
-accountID = "101-011-6626734-003"
-OANDA_URL = "https://api-fxpractice.oanda.com/v3"
-
-SECURE_HEADER = {"Authorization": f"Bearer {API_KEY}"}
